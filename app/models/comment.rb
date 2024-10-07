8f16c73e565afc96e685417abf33e539f96fc574
test.txtにテキトーな文章を追加 <>
2024-10-07 14:40:47 +0900

    usersテーブルとcommentsテーブルにそれぞれ不足していたカラムを追加してマイグレーションを実行

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,7 @@
 # Table name: comments
 #
 #  id         :bigint           not null, primary key
+#  body       :text
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
