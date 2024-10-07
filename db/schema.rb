ebb225adaf3689cee92a8a18fb71fed67cfd9606
test.txtにテキトーな文章を追加 <>
2024-10-07 14:50:10 +0900

    それぞれのテーブルにuser_idを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_10_07_053733) do
+ActiveRecord::Schema.define(version: 2024_10_07_054744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 2024_10_07_053733) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "body"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -53,4 +55,5 @@ ActiveRecord::Schema.define(version: 2024_10_07_053733) do
 
   add_foreign_key "article_likes", "articles"
   add_foreign_key "article_likes", "users"
+  add_foreign_key "comments", "users"
 end
