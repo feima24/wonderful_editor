814adb1bd5eab7f24203802939b1ac24c7d8f66c
test.txtにテキトーな文章を追加 <>
2024-10-16 20:04:15 +0900

    記事詳細の実装

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -11,7 +11,6 @@ RSpec.describe "Api::V1::Articles", type: :request do
     it "記事の一覧が取得できる" do # rubocop:disable RSpec/MultipleExpectations
       subject
       res = JSON.parse(response.body)
-
       expect(response).to have_http_status(:ok)
       expect(res.length).to eq 3
       expect(res.map {|d| d["id"] }).to eq [article3.id, article1.id, article2.id]
